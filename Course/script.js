634ef026c42be01a9ae6fbfc4f26051b7b1debe8
PedroBuffon <henriquebuffon@gmail.com>
2022-11-21 14:22:34 -0300

    Update

--- Course/script.js
+++ Course/script.js
@@ -8,3 +8,73 @@ let firstName = "Pedro";
 
 console.log(firstName);
 */
+
+// Lecture 2
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 24);
+console.log(typeof 'Pedro');
+
+javascriptIsFun = "YES!!!";
+
+console.log(typeof javascriptIsFun);
+
+let year;
+
+console.log(year);
+console.log(typeof year);
+
+year = 1997;
+
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+// Lecture 3
+/*
+let age = 30;
+age = 31;
+
+const birthYear = 1997;
+// birthYear = 1998;
+// const job;
+
+var job = "Programmer";
+job = "Teacher";
+*/
+
+// Lecture 4
+// Math Operators
+/*
+const now = 2022;
+const agePedro = now-1997;
+const ageThiago = now-2003;
+console.log(agePedro, ageThiago);
+
+console.log(agePedro * 2, agePedro / 2, 2**3);
+
+const firstName = "Pedro";
+const lastName = "Buffon";
+
+console.log(firstName+' '+lastName);
+
+// Assignment operators
+let x = 10 + 5;
+x+= 10;
+x *= 4;
+x++;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(agePedro > ageThiago);
+console.log(ageThiago >= 18);
+*/
+
+// Lecture 5

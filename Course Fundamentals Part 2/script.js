980a904978a11f560203e7a3e4f1983c003b4237
PedroBuffon <henriquebuffon@gmail.com>
2022-12-06 10:28:55 -0300

    Update

---
+++ Course Fundamentals Part 2/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Lecture 1 strict mode
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log('i can drive');
+*/
+
+// Lecture 2 Functions

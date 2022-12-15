3d0193148fe1c9c7d3f28ab16856dccc08c23d40
PedroBuffon <henriquebuffon@gmail.com>
2022-12-15 09:46:47 -0300

    Update

--- Course Fundamentals Part 2/script.js
+++ Course Fundamentals Part 2/script.js
@@ -10,3 +10,23 @@ if (hasDriversLicense) console.log('i can drive');
 */
 
 // Lecture 2 Functions
+
+function logger(){
+    console.log('My name is Pedro');
+}
+//calling / running / invoking
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges){
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const applejuice = fruitProcessor(5,0);
+console.log(applejuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
